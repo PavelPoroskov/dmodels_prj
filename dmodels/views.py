7d8f5279ec17c306ba606d882634a866eea8ea9d
PavelPoroskov <pavl1853@gmail.com>
2014-09-14 00:43:42 +0400

    done serialize

--- dmodels/views.py
+++ dmodels/views.py
@@ -7,10 +7,62 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET, require_POST
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+from django.db.models import Model as DjangoBaseModel
 
 
 from dmodels.models import dictModelStructure
 from dmodels import models
+
+# data -- QueryView, obj
+def serialize( data ):
+
+	def object_to_arr( obj, listFields ):
+		new_row = []
+		for field in listFields:
+			field_id = field['id']
+			value = getattr( obj, field_id )
+
+			new_row.append( value )
+
+		return new_row
+
+
+	model_name = ""
+	isOneObj = False
+	if issubclass( data.__class__, DjangoBaseModel ):
+		isOneObj = True
+		model_name = data.__class__.__name__
+	else:  # QuerySet
+		model_name = data.model.__name__
+	Model = getattr( models, model_name )
+	listFields = dictModelStructure[model_name]['fields'] 
+
+
+	new_data = []
+#	if isinstance( data, Model ):
+	if isOneObj:
+		new_row = object_to_arr( data, listFields )
+		new_data.append( new_row )
+	else: # QuerySet
+		for row in data:
+			new_row = object_to_arr( row, listFields )				
+			new_data.append( new_row )
+
+
+	res_str = json.dumps({
+		'model': model_name,
+		'fields': listFields,
+		'data': new_data
+		})
+
+	return res_str
+
+def deserialize( model_name, request_POST ):
+	dictFilds = dictModelStructure[model_name]['dictFilds'] 
+	dictValues = { field: int(value) if dictFilds[field]['type'] == 'int' else value 
+					for (field, value) in request_POST.items() } 
+
+	return dictValues
 
 
 @ensure_csrf_cookie
@@ -24,28 +76,10 @@ def index(request):
 
 def ajax_get_list(request, model_name):
 
-	listFields = dictModelStructure[model_name]['fields'] 
-
 	Model = getattr( models, model_name )
 	list_rows = Model.objects.all()
 
-	new_data = []
-	for row in list_rows:
-		new_row = []
-		for field in listFields:
-			field_id = field['id']
-			value = getattr( row, field_id )
-
-			new_row.append( value )
-			
-		new_data.append( new_row )
-
-
-	res_str = json.dumps({
-		'model': model_name,
-		'fields': listFields,
-		'data': new_data
-		})
+	res_str = serialize( list_rows )
 	
 	return HttpResponse(res_str)
 
@@ -53,13 +87,12 @@ def ajax_get_list(request, model_name):
 @csrf_protect
 def ajax_change( request, model_name, row_id ):
 
-#	obj = dictModelClasses[model_name].objects.get( **{'id': row_id} )
+	dictValues = deserialize( model_name, request.POST )
+
 	Model = getattr( models, model_name )
 	obj = Model.objects.get( **{'id': row_id} )
-
-	for (field, value) in request.POST.items():
+	for (field, value) in dictValues.items():
 		setattr( obj, field, value )
-
 	obj.save()
 
 	return HttpResponse("Ok")
@@ -69,36 +102,13 @@ def ajax_change( request, model_name, row_id ):
 @csrf_protect
 def ajax_add( request, model_name ):
 
-#	dictValues = {}
-#	for (field, value) in request.POST.items(): 
-#		dictValues[field] = value;
-#	dictValues = { field: value for (field, value) in request.POST.items() } 
-	dictFilds = dictModelStructure[model_name]['dictFilds'] 
-	dictValues = { field: int(value) if dictFilds[field]['type'] == 'int' else value for (field, value) in request.POST.items() } 
+	dictValues = deserialize( model_name, request.POST )
 
-#	print(dictValues)
-
-#	obj = dictModelClasses[model_name]( **dictValues )
 	Model = getattr( models, model_name )
-#	obj = Model( **request.POST )
 	obj = Model( **dictValues )
 	obj.save()
 
-
-	listFields = dictModelStructure[model_name]['fields'] 
-	new_row = []
-	for field in listFields:
-		field_id = field['id']
-		value = getattr( obj, field_id )
-		new_row.append( value )
-
-
-	res_str = json.dumps({
-		'model': model_name,
-		'data': [new_row]
-		})
-#	print(new_row)
-#	print(res_str)
+	res_str = serialize( obj )
 
 	return HttpResponse(res_str)
 
